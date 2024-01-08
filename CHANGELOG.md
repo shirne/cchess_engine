05e25d861684cd79b470d864b5353db4f1f88a28
shirne <shirne@126.com>
2024-01-08 11:23:47 +0800

    fix duplicate logger listener

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.4
+* fix duplicate logger listener
+
 ## 0.0.3
 * add logger control
 
--- lib/src/tools.dart
+++ lib/src/tools.dart
@@ -2,23 +2,33 @@ import 'dart:developer';
 
 import 'package:logging/logging.dart';
 
+final logger = _getLogger('CCHESS_ENGINE');
+
 /// override logger level
 Level _loggerLevel = Level.ALL;
 
-final logger = Logger('CCHESS_ENGINE')
-  ..onRecord.listen((record) {
-    if (_loggerLevel.value > record.level.value) {
-      return;
-    }
-    log(
-      record.message,
-      time: record.time,
-      level: record.level.value,
-      error: record.error,
-      stackTrace: record.stackTrace,
-      sequenceNumber: record.sequenceNumber,
-    );
-  });
+bool _loggerListened = false;
+
+Logger _getLogger(String name) {
+  final logger = Logger('CCHESS');
+  if (!_loggerListened && hierarchicalLoggingEnabled) {
+    _loggerListened = true;
+    logger.onRecord.listen((record) {
+      if (_loggerLevel.value > record.level.value) {
+        return;
+      }
+      log(
+        record.message,
+        time: record.time,
+        level: record.level.value,
+        error: record.error,
+        stackTrace: record.stackTrace,
+        sequenceNumber: record.sequenceNumber,
+      );
+    });
+  }
+  return logger;
+}
 
 /// control package
 class CChessEngine {
