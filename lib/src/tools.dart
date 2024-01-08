bf86b35221ae3158c90b744a0c92deceb2c057c3
shirne <shirne@126.com>
2024-01-08 09:43:18 +0800

    add logger control

--- lib/src/tools.dart
+++ lib/src/tools.dart
@@ -2,8 +2,14 @@ import 'dart:developer';
 
 import 'package:logging/logging.dart';
 
+/// override logger level
+Level _loggerLevel = Level.ALL;
+
 final logger = Logger('CCHESS_ENGINE')
   ..onRecord.listen((record) {
+    if (_loggerLevel.value > record.level.value) {
+      return;
+    }
     log(
       record.message,
       time: record.time,
@@ -13,3 +19,21 @@ final logger = Logger('CCHESS_ENGINE')
       sequenceNumber: record.sequenceNumber,
     );
   });
+
+/// control package
+class CChessEngine {
+  /// close log of cchess package
+  static void closeLog() {
+    _loggerLevel = Level.OFF;
+  }
+
+  /// default to log info level
+  static void openLog() {
+    _loggerLevel = Level.INFO;
+  }
+
+  /// set log level of cchess package
+  static set logLevel(Level level) {
+    _loggerLevel = level;
+  }
+}
